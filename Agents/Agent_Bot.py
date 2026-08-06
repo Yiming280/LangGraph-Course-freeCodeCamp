6b5b9f04faa3daa27ade8bca81f5d6e71916414f
Yiming280 <weiyimingde@163.com>
2026-08-06 11:19:13 +0800

    use remote qwen3.6 & add system prompt

--- Agents/Agent_Bot.py
+++ Agents/Agent_Bot.py
@@ -1,19 +1,38 @@
 from typing import TypedDict, List
-from langchain_core.messages import HumanMessage
+from urllib import response
+from langchain_core.messages import SystemMessage, HumanMessage
 from langchain_ollama import ChatOllama
 from langgraph.graph import StateGraph, START, END
-from dotenv import load_dotenv # used to store secret stuff like API keys or configuration values
+from dotenv import load_dotenv
+from numpy import rint # used to store secret stuff like API keys or configuration values
 
 load_dotenv()
 
 class AgentState(TypedDict):
     messages: List[HumanMessage]
 
-llm = ChatOllama(model="qwen2.5")
+llm = ChatOllama(
+    base_url="http://10.8.20.83:11435",  # 远程 Ollama 服务器地址和映射端口
+    model="qwen3.6:27b",                # 模型名称
+    temperature=0.2,
+)
+
+system_prompt = SystemMessage(content=f"""
+   你是一个具备深厚逻辑推理能力的大语言模型。
+    无论用户提出什么问题，你都【必须】严格按照以下格式回答：
+
+    <think>
+    在这里写下你的详细思考过程、逻辑拆解、边界条件分析与方案对比。
+    </think>
+
+    在这里给出最终对用户展示的正式回答。""")
 
 def process(state: AgentState) -> AgentState:
-    response = llm.invoke(state["messages"])
-    print(f"\nAI: {response.content}")
+    # response = llm.invoke(state["messages"])
+    # print("思维链:", response.additional_kwargs.get("reasoning_content"))
+    for chunk in llm.stream(state["messages"]):
+        print(chunk.content, end="", flush=True)
+    # print(f"\nAI: {response.content}")
     return state
 
 graph = StateGraph(AgentState)
@@ -24,5 +43,7 @@ agent = graph.compile()
 
 user_input = input("Enter: ")
 while user_input != "exit":
-    agent.invoke({"messages": [HumanMessage(content=user_input)]})
-    user_input = input("Enter: ")
+    user_message = HumanMessage(content=user_input)
+    all_messages = [system_prompt] + [user_message]
+    agent.invoke({"messages": all_messages})
+    user_input = input("\nEnter: ")
